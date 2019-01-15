baa46c01518ddead2dfba949b47646955d5264d5
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-01-15 16:35:43 -0500

    deleted width information from ImpactItems. Not relevant information

--- P2/sentry-app/src/components/ImpactItem/index.js
+++ P2/sentry-app/src/components/ImpactItem/index.js
@@ -3,13 +3,13 @@ import './style.css'
 import util from '../../util'
 
 const ImpactItem = props => {
-  const {des, date, ip, width} = props.impactor
+  const {des, date, ip} = props.impactor
   const dateText = util.parseDateString(date)
   const prob = (util.convertExpToDec(ip) * 100).toFixed(3)
+
   return (
     <div className="impact-item">
       <p className="impact-item__heading"><span className="impact-item__em">Object Name:</span> {des}</p>
-      <p className="impact-item__info"><span className="impact-item__em">Diameter:</span> {width}</p>
       <p className="impact-item__info"><span className="impact-item__em">Impact Date:</span> {dateText}</p>
       <p className="impact-item__info"><span className="impact-item__em">Impact Probability:</span> {prob}%</p>
     </div>
