a5efbd90034aec515aef515d0cb8fff5b55754ca
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-01-17 23:00:29 -0500

    moved header away from window edge in wide screen

---
+++ P2/sentry-app/src/util/index.js
@@ -0,0 +1,22 @@
+const helper = {
+  months: ['January','February','March','April','May','June','July','August','September','October','November','December'],
+
+  parseDateString(str) {
+    const arr = str.split('-')
+    const year = arr[0]
+    const month = this.months[arr[1] -1]
+    const day = arr[2].split('.')[0]
+    return `${month} ${day}, ${year}`
+  },
+
+  convertExpToDec(str) {
+    const arr = str.toLowerCase().split('e')
+    const base = Number(arr[0])
+    const neg = arr[1][0] === '-'
+    const expStr = neg ? arr[1].slice(1) : arr[1]
+    const exp = neg ? -Number(expStr) : Number(expStr)
+    return base * Math.pow(10, exp)
+  },
+}
+
+export default helper
